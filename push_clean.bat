f8f1c920ddbe1505517a25d506855ba245ace0a9
hadenawi-blip <hadenawi@gmail.com>
2026-05-28 19:42:26 +0300

    chore: remove push_clean.bat

--- push_clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d C:\Projects\qisas-app
-git rm --cached commit_msg.txt do_commit.bat 2>nul
-del commit_msg.txt 2>nul
-del do_commit.bat 2>nul
-git add -A
-git commit -m "chore: remove temp commit helper files"
-git push origin main --tags
-echo PUSH_EXIT:%ERRORLEVEL%
